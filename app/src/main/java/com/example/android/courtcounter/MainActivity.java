72c9ff13633676742fc8d6d22d7feab56a8862cf
Jashaswee Jena <jishujena2000@gmail.com>
2016-05-05 00:25:24 +0530

    Initial Commit!!!

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+int scoreTeamA=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+  public void addThreeForTeamA(View view)
+  {
+      scoreTeamA=scoreTeamA+3;
+      displayForTeamA(scoreTeamA);
+
+  }
+
+    public void addTwoForTeamA(View view)
+    {
+        scoreTeamA=scoreTeamA+2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamA(View view)
+    {
+
+        scoreTeamA=scoreTeamA+1;
+        displayForTeamA(scoreTeamA);
+    }
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
+
